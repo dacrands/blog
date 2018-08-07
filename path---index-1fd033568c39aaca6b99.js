7c5661a80303abbc3fea1d5e69bac00eea8eeaf9
da crands <davecrands@gmail.com>
2018-08-06 22:29:13 -0400

    Updates

---
+++ path---index-1fd033568c39aaca6b99.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{331:function(t,o){t.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{title:"First Blog Post",path:"/first-post"}}},{node:{frontmatter:{title:"Second Blog Post",path:"/second-post"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-1fd033568c39aaca6b99.js.map
---
+++ path---second-post-5c9c8cde234df25177b8.js
@@ -0,0 +1,2 @@
+webpackJsonp([26807536709107],{333:function(t,o){t.exports={data:{markdownRemark:{html:"<h1>Hello!</h1>\n<p>This is the first blog post I made\nwith Gatsby.</p>",frontmatter:{path:"/second-post",title:"Second Blog Post"}}},pathContext:{}}}});
+//# sourceMappingURL=path---second-post-5c9c8cde234df25177b8.js.map
